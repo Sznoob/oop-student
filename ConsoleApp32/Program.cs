d5ec09b5a774c91c362309962dbb1f4f287ebdf1
Joni Kananen <L7043@student.jamk.fi>
2018-01-21 15:05:03 +0200

    Student oop

--- ConsoleApp32/Program.cs
+++ ConsoleApp32/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp32
     {
         static void Main(string[] args)
         {
+            StudentDetails details = new StudentDetails();
+            details.PrintData();
         }
     }
 }
---
+++ ConsoleApp32/StudentDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    class StudentDetails
+    {
+        Student numb1 = new Student("Pavol", "Smith", 8.5f);
+        Student numb2 = new Student(13, "Alex ", "Leeroy", 6.3f);
+
+        public void PrintData()
+        {
+            Console.WriteLine("{0} {1}'s GPA is {2}", numb1.FirstName, numb1.LastName, numb1.GPA);
+            Console.WriteLine("Student ID is {0}. {1} {2}'s GPA is {3}", numb2.StudentId, numb2.FirstName, numb2.LastName, numb2.GPA);
+        }  
+        
+        
+
+    }
+}
